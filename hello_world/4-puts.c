19e6c6641fde306010c171485092ab84e1e4320b
eno007 <eno.gupe@gmail.com>
2022-06-06 06:34:42 -0700

    Task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main ()
-{ 
-	puts ""Programming is like building a multilingual puzzle";
-		return 0;
+/**
+ * main -- entry point
+ * prints a given messagge
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
