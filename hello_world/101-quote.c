5ccffa8670c19601ec0545f35327c4ee475f3c92
eno007 <eno.gupe@gmail.com>
2022-06-06 07:33:35 -0700

    Task 8

---
+++ hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -- entry point
+ * prints the size of various types on the computer it is compiled and run on.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
