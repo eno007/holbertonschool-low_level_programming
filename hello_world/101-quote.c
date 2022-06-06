b40ce2ea6915030be26d20732ec54668dd4e9539
eno007 <eno.gupe@gmail.com>
2022-06-06 14:05:26 -0700

    Task 7

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main -- entry point
- * prints the size of various types on the computer it is compiled and run on.
- * Return: Always 0 (Success)
+ * .
+ * Return: Always 1
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
