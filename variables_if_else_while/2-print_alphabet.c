60e318cf0b3ccd489ff1627cb463894346531c68
eno007 <eno.gupe@gmail.com>
2022-06-07 06:28:55 -0700

    Task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -- entry point
+ * prints the size of various types on the computer it is compiled and run on.
+ * Return: Always 0 (Success)
+ */
+char main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
