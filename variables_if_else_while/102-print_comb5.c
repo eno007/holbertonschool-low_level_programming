0323dde0db5bbc5925413b1847eb366bf5ed90cd
eno007 <eno.gupe@gmail.com>
2022-06-07 14:30:56 -0700

    Task 102

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -- entry point
+ * prints the size of various types on the computer it is compiled and run on.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	int N;
+	int M;
+	int m;
+
+	for (N = '0'; N <= '9'; N++)
+	{
+		for (n = '0'; n <= '9'; n++)
+		{
+			for (m = N ; m <= '9'; m++)
+			{
+				for (M = n + 1; M <= '9'; M++)
+				{
+					putchar(N);
+					putchar(n);
+					putchar(' ');
+					putchar(M);
+					putchar(m);
+
+					if (!((N == '9' && n == '8') && (M == '9' && m == '9')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				m = '0';
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
