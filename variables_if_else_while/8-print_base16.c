393636f327f7356bd32bb2345917973c1a593df6
eno007 <eno.gupe@gmail.com>
2022-06-07 08:10:10 -0700

    Task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main -- entry point
+ * prints the size of various types on the computer it is compiled and run on.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '1'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (n = 'a'; n <= 'f'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
