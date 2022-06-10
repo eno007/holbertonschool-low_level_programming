e4cdcc567ab6555a69a0555b2bb0930679ea605e
eno007 <eno.gupe@gmail.com>
2022-06-10 02:58:32 -0700

    Task 2

--- debugging/main.h
+++ debugging/main.h
@@ -1,6 +1,7 @@
-#ifndef main.h
-#define main.h
+#ifndef main_H
+#define main_H
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
 #endif /* MAIN_H */
