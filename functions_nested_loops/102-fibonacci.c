5fa6bd3f7bbbd21bc71704fe4d3e21fbcca959d6
eno007 <eno.gupe@gmail.com>
2022-06-10 05:58:55 -0700

    Task 102

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -7,17 +7,17 @@ int main(void)
 {
 	long f1 = 1;
 	long f2 = 2;
+	long fn;
 	int num;
-	int limit = 50;
-
-	for (num = 1; num <= (limit / 2); num++)
+	
+	printf("%li, %li", f1, f2);
+	for (num = 1; num <= 51; num++)
 	{
-		printf("%li, %li", f1, f2);
-		f1 += f2;
-		f2 += f1;
+		fn = f1 + f2;
+		printf(", %li", fn);
+		f1 = f2;
+		f2 = fn;
 	}
-	if (limit % 2 == 1)
-		printf("%li, ", f1);
 	printf("\n");
 
 	return (0);
