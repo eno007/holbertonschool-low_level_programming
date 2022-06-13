c2ccf196e1f1cb9e1a1eb1275b1797d97c431dcb
eno007 <eno.gupe@gmail.com>
2022-06-13 04:57:42 -0700

    Task 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,12 +8,17 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		_putchar(' ');
-		if (i == n)
-			_putchar('\\');
+		for (j = 0; j <= i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
 	}
-	_putchar('\n');
+	if (i < 0)
+		_putchar('\n');
 }
