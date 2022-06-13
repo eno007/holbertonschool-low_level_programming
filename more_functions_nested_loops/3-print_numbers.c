d781affd45b1f28aa923f811a3781eb86ffbd0c2
eno007 <eno.gupe@gmail.com>
2022-06-13 02:58:17 -0700

    Task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code.
+ * Return:  0 or 1.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
