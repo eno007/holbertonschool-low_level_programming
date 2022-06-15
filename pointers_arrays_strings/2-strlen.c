9b5aa99871cba9491912c4fd146fdbcfd35c93a1
eno007 <eno.gupe@gmail.com>
2022-06-15 03:07:23 -0700

    Task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - check the code
+ * @s: to check
+ * Return: len
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
