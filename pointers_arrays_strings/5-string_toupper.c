d453a8d3c86039cea514184de7bbcfaf2055f03e
eno007 <eno.gupe@gmail.com>
2022-06-16 04:29:00 -0700

    Task 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *string_toupper - check the code
+ * @s: to check
+ * Return: len
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+	}
+	return (s);
+}
