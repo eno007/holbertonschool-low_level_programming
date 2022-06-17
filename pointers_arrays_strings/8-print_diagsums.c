1b436518d5c51901db57e830cba6960d57f37307
eno007 <eno.gupe@gmail.com>
2022-06-17 14:47:15 -0700

    Task 8

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - check the code
+ * @a: to check
+ * @size: to check
+ * Return: required output
+ */
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int j = 0;
+	int m, n;
+
+	for (m = 0; i < size; m++)
+	{
+		n = (m * size) + m;
+		i = i + a[n];
+	}
+	for (m = 1; m <= size; m++)
+	{
+		n = (m * size) - m;
+		j = j + a[n];
+	}
+	printf("%d, %d\n", i, j);
+}
