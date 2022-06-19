61ad255be8ce59991fe1d32b8c568bb78511dc3a
eno007 <eno.gupe@gmail.com>
2022-06-19 13:15:39 -0700

    Task 100

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - check the code
+ * @s: to check
+ * @to: to check
+ * Return: required output
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
