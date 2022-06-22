86cc66331ad3f18c60037b4fe56132826b0d204e
eno007 <eno.gupe@gmail.com>
2022-06-22 02:57:11 -0700

    Task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - enters program
+ * @argc: The character to check
+ * @argv: The character to print
+ * Return: On success 1.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
