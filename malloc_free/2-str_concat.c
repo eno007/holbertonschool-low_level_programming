c62d7d0903e4075661928ee467bca31b734245f0
eno007 <eno.gupe@gmail.com>
2022-06-23 05:45:50 -0700

    Task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,9 +14,9 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, length = 0, length1 = 0;
 
 	if (s1 == NULL)
-		return ("");
+		s1 = "";
 	if (s2 == NULL)
-		return ("");
+		s2 = "";
 	while (*(s1 + i))
 	{
 		length++;
