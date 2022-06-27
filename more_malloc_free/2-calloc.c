e78715b4106fc68ad1c06cb41034ef12d866a738
eno007 <eno.gupe@gmail.com>
2022-06-27 05:24:17 -0700

    Task 2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - enters program
+ * @nmemb: The character to check
+ * @size: The character to check
+ * Return: On success array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *array;
+	unsigned int i;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+	array = malloc(nmemb * size);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * size;  i++)
+		*((char*)array + i) = 0;
+	return (array);
+}
