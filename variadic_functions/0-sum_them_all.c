3bc908db1c3577750397e86c599516a3c560a0e5
eno007 <eno.gupe@gmail.com>
2022-07-04 01:00:00 -0700

    updating task 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -10,6 +10,7 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list variadic_list;
 	unsigned int tot, i;
+
 	tot = 0;
 
 	if (n == 0)
@@ -18,5 +19,5 @@ int sum_them_all(const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 		tot += va_arg(variadic_list, int);
 	va_end(variadic_list);
-	return(tot);
+	return (tot);
 }
