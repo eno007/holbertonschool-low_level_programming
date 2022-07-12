338a86c9ad09ded52418a511c3c06633a33e4f68
eno007 <eno.gupe@gmail.com>
2022-07-12 05:52:27 -0700

    Task 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - structure to print
+ * @h: linked list
+ * Return: number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
