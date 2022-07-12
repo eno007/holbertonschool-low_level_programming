def94d1088f5db22e92b5e7db47d811d671aa747
eno007 <eno.gupe@gmail.com>
2022-07-12 05:47:37 -0700

    lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,8 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -11,7 +10,7 @@
  *
  * Description: singly linked list node structure
  */
-typedef struct lists_s
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
