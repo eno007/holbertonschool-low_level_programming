08770cee0086333f36d948b5c1e02ff820f45cbf
eno007 <eno.gupe@gmail.com>
2022-07-13 03:51:08 -0700

    Task 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_list - structure to print
+ * @head: linked list pointer
+ * 
+ */
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		free(head->str);
+		free(head);
+		head = head->next;
+	}
+}
