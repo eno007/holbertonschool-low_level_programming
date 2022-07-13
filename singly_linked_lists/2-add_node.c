0cb4ca25918a32f496b59cd7a45774831b1f0a49
eno007 <eno.gupe@gmail.com>
2022-07-13 02:22:50 -0700

    Task 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * _strlen - structure to print
+ * @str: new list
+ * Return: length of string
+ */
+int _strlen(const char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+	return (len);
+}
+/**
+ * add_node - structure to print
+ * @head: linked list pointer
+ * @str: new list
+ * Return: address of new element or NULL if it fails
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	if (str == NULL)
+		return (NULL);
+	/*if (strdup(str) == NULL)
+		return (NULL);*/
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	if (head == NULL)
+		new->next = NULL;
+	else
+		new->next = *head;
+	*head = new;
+	return (new);
+}
