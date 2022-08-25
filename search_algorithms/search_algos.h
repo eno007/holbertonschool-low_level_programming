88ac0de2f7a28a9e93d57375f4dbb9c0320c71f0
eno007 <eno.gupe@gmail.com>
2022-08-25 01:45:00 -0700

    update of given files

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <string.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
