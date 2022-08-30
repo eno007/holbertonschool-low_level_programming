2455437833165efe5e05acdb001f2c4819472802
eno007 <eno.gupe@gmail.com>
2022-08-30 14:09:23 -0700

    update task

--- search_algorithms/100-jump.c
+++ search_algorithms/100-jump.c
@@ -33,7 +33,7 @@ int jump_search(int *array, size_t size, int value)
 		}
 	}
 
-	for (; position < size; position++)
+	for (position = 0; position < size; position++)
 	{
 		printf("Value checked array[%d] = [%d]\n",
 		       (int)position, array[position]);
